2a5777a4e3c46f13fece6a2f12cf332aa3a0daf7
Student <you@example.com>
2019-04-03 18:50:30 -0700

    Started from beginning my CRUD webpage

--- app/app.js
+++ app/app.js
@@ -1,109 +1,39 @@
-var loadLocalStorage = function () {
-	var keys = Object.keys(localStorage)
-	var htmlString = '';
-	for (var i = 0; i < keys.length; i++) {
-		htmlString += `<tr><td>${keys[i]}</td><td>${localStorage[keys[i]]}</tr></tr>`;
-	}
-	$('tbody').html(htmlString)
-};
-
-var updateStatusLabel = function(message) {
-	$('#statusLabel').text('Status: ' + message);
-}
-
- //jQuery document ready initialization stuff
- ////button and form event handlers
- // logic for determining action probably needs to go in the event handler
+// Strike through functionality
 $(document).ready(function () {
-	loadLocalStorage();
+ $("ul").on("click", "li", function() {
+ 	$(this).toggleClass("done");
 
-	$('#btn-create').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
 
-		if (keyExists) {
-			updateStatusLabel('key already exists, please use update button instead! :D');
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		}else {
-			createEntry(key, value);
-			updateStatusLabel('key created - ' + key);
-		}
+  });
 
-		loadLocalStorage();
+
+// delete
+
+$("ul").on("click", "span", function(event) {
+
+	$(this).parent().fadeOut(300, function() {
+		$(this).remove();
 	});
-
-	$('#btn-update').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var existingValue = localStorage.getItem(key)
-		var keyExists = existingValue !== null;
-
-		if (value === existingValue) {
-			updateStatusLabel('key not updated - that value already exists silly! xD')
-		} else if (keyExists) {
-			updateEntry(key, value);
-			updateStatusLabel('key updated - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
-		}		
-		
-		loadLocalStorage();		
-	});
-
-	$('#btn-delete').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
-
-		if (keyExists) {
-			removeEntry(key);
-			updateStatusLabel('key removed - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, nothing removed. :|');
-		}
-
-		loadLocalStorage();
-	});	
+	event.stopPropagation();
 
 });
-/*
 
 
+// add
 
-When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
+$("input").keypress(function(event){
+	if(event.which === 13) {
+		var task = $("input").val();
+		$("ul").append("<li><span class='delete'><i class='fas fa-angry'></i></span>"+ task + "</li>")
+		$("input").val("");
+	}
+});
 
-let form = document.querySelector("form");
-let guestName = form.elements.guest;
-let hatSize = form.elements["hat-size"];
-*/
+// Toggling the plus
 
-/*
-PAGE CONTENT STUFF
-*/
-//something to update the table every time localStorage changes
+$("#plus").on("click", function() {
+	$("input").fadeToggle();
+})
 
-//localStorage stuff
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-////create new entry
-//localStorage.setItem(key, value)
-var createEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
 
-////Update existing entry
-//localStorage.setItem(key, value)
-var updateEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
-
-////delete existing entry
-//localStorage.removeItem(key)
-var removeEntry = function(key) {
-	return localStorage.removeItem(key);
-}
+});
